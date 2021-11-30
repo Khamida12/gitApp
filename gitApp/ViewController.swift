89a236b2bcdfb51e908d60a181475d13d81d8dd3
Tima <timokha_98@mail.ru>
2021-11-30 12:18:17 +0600

    ver1

--- gitApp/ViewController.swift
+++ gitApp/ViewController.swift
@@ -13,7 +13,82 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
+final class LightOutsid{
+    var intensity: Double = 1.0
+    
+    func switchOn(){
+        print("Light switched on")
+    }
+    
+    func switchOff(){
+        print("Light switched off")
+    }
 
 }
 
+final class HeatingCooling{
+    var temperature: Double = 21.5
+    var mode: String {
+        return temperature >= 25 ? "heating" : "cooling"
+    }
+    
+    func start(){
+        print("Start \(mode)")
+    }
+    
+    func stop(){
+        print("Stop \(mode)")
+    }
+}
+
+protocol Command{
+    func execute()
+}
+
+final class SwitchOnLightCommand: Command {
+    var light: LightOutsid
+    
+    init(light: LightOutsid){
+        self.light = light
+    }
+    
+    func execute(){
+        light.switchOn()
+    }
+}
+
+final class StartHeatingCommand: Command {
+    var heater: HeatingCooling
+    
+    init(heater: HeatingCooling){
+        self.heater = heater
+    }
+    
+    func execute(){
+        if heater.temperature < 25 {
+            heater.temperature = 25
+        }
+        heater.start()
+    }
+}
+
+final class Program{
+    var commands: [Command] = []
+    
+    func start() {
+        commands.forEach { $0.execute() }
+    }
+}
+
+let light = LightOutsid()
+let heater = HeatingCooling()
+
+let lightOnCommand = SwitchOnLightCommand(light: light)
+let heatCommand = StartHeatingCommand(heater: heater)
+
+let eveningProgram = Program()
+eveningProgram.commands.append(lightOnCommand)
+eveningProgram.commands.append(heatCommand)
+eveningProgram.start()
